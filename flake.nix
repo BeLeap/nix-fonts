56131e1b7ffb30d3e7d0086d536ec35de1385fcd
BeLeap <beleap@beleap.dev>
2025-11-05 01:01:38 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -7,32 +7,15 @@
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      rec {
-        packages.nanum-myeongjo = pkgs.stdenvNoCC.mkDerivation {
-          name = "nanum-myeongjo";
-          dontConfigue = true;
-          dontUnpack = true;
-          src = pkgs.fetchurl {
-            url = "https://hangeul.pstatic.net/hangeul_static/webfont/NanumMyeongjo/NanumMyeongjo.ttf";
-            hash = "sha256-60qd4wIu0nRMBipMRTMuh3FUYr9y817cNP6EUptKwT0=";
-          };
-          installPhase = ''
-            mkdir -p $out/share/fonts/truetype
-            cp $src $out/share/fonts/truetype/
-          '';
-        };
-
-        overlay = prev: final: {
-          nanum-myeongjo = packages.nanum-myeongjo;
-        };
-      }
-    );
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "beleap-nix-fonts";
+      overlay = ./overlay.nix;
+    };
 }
